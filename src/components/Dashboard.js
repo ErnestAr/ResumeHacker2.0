93227b905baa5bd4a05c18284e20f3ab1d1f7ac5
ErnestAr <arutiunian.ernest@gmail.com>
2021-08-25 12:56:02 -0400

    images

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -101,7 +101,7 @@ export default function Templates() {
                       <CardActionArea>
                         <CardMedia
                           className={classes.media}
-                          image="/images/templategreen.png"
+                          image="public/images/templategreen.png"
                           title="Contemplative Reptile"
                         />
                         <CardContent>
@@ -126,7 +126,7 @@ export default function Templates() {
                         <CardActionArea>
                           <CardMedia
                             className={classes.media}
-                            image="/images/templateblue.png"
+                            image="public/images/templateblue.png"
                             title="Contemplative Reptile"
                           />
                           <CardContent>
